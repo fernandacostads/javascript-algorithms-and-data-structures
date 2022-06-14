f455ade08caed3190748a6372f9e7f91ff4c2018
Fernanda Costa <fernandacosta@alu.ufc.br>
2022-06-13 21:52:49 -0300

    (add): OOP 26/26

--- object-oriented-programming/understand-own-properties.js
+++ object-oriented-programming/understand-own-properties.js
@@ -30,3 +30,18 @@ You should solve this challenge without using the built in method Object.keys().
 You should solve this challenge without hardcoding the ownProps array.
 */
 
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+
+for (let property in canary) {
+  if(canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
