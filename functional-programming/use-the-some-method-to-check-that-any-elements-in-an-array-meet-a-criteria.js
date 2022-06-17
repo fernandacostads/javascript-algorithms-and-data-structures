ccad1d10e9dc3c2fcc4cca8f3f9aee0c61a0462c
Fernanda Costa <fernandacosta@alu.ufc.br>
2022-06-17 18:39:59 -0300

    (add): functional programming 24/24

---
+++ functional-programming/use-the-some-method-to-check-that-any-elements-in-an-array-meet-a-criteria.js
@@ -0,0 +1,33 @@
+/*
+The some method works with arrays to check if any element passes a particular test. It returns a Boolean value - true if any of the values meet the criteria, false if not.
+
+For example, the following code would check if any element in the numbers array is less than 10:
+
+const numbers = [10, 50, 8, 220, 110, 11];
+
+numbers.some(function(currentValue) {
+  return currentValue < 10;
+});
+The some method would return true.
+
+Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+
+Tests
+Waiting:Your code should use the some method.
+Waiting:checkPositive([1, 2, 3, -4, 5]) should return true.
+Waiting:checkPositive([1, 2, 3, 4, 5]) should return true.
+Waiting:checkPositive([-1, -2, -3, -4, -5]) should return false.
+*/
+
+function checkPositive(arr) {
+  return arr.some((currentValue) => {
+    return currentValue > 0;
+  });
+}
+
+checkPositive([1, 2, 3, -4, 5]);
+
+function checkPositive(arr) {
+  return arr.some(elem => elem > 0);
+}
+checkPositive([1, 2, 3, -4, 5]);
